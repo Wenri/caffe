d5d93051d2f0a957590b653c91c6fd92adb31963
wenri <wenri@charlie.cs.columbia.edu>
2016-02-23 12:57:23 -0500

    Training start

--- examples/cifar10/train_circ.sh
+++ examples/cifar10/train_circ.sh
@@ -5,12 +5,12 @@ TOOLS=./build/tools
 $TOOLS/caffe.bin train \
     --solver=examples/cifar10/cifar10_circ_solver.prototxt
 
-# # reduce learning rate by factor of 10
-# $TOOLS/caffe.bin train \
-#     --solver=examples/cifar10/cifar10_full_solver_lr1.prototxt \
-#     --snapshot=examples/cifar10/cifar10_full_iter_60000.solverstate.h5
+# reduce learning rate by factor of 10
+$TOOLS/caffe.bin train \
+    --solver=examples/cifar10/cifar10_circ_solver_lr1.prototxt \
+    --snapshot=examples/cifar10/cifar10_circ_iter_60000.solverstate.h5
 
-# # reduce learning rate by factor of 10
-# $TOOLS/caffe.bin train \
-#     --solver=examples/cifar10/cifar10_full_solver_lr2.prototxt \
-#     --snapshot=examples/cifar10/cifar10_full_iter_65000.solverstate.h5
+# reduce learning rate by factor of 10
+$TOOLS/caffe.bin train \
+    --solver=examples/cifar10/cifar10_circ_solver_lr2.prototxt \
+    --snapshot=examples/cifar10/cifar10_circ_iter_65000.solverstate.h5
