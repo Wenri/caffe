1f40ec1453d2c0e31b01b8d76d4928908e7cbfa2
xsnow <xsnow@outlook.com>
2016-05-18 15:18:12 -0400

    Skew circ modify

--- examples/cifar10/train_circ.sh
+++ examples/cifar10/train_circ.sh
@@ -8,11 +8,11 @@ $TOOLS/caffe.bin train \
 exit 0
 
 # reduce learning rate by factor of 10
-$TOOLS/caffe.bin train \
-    --solver=examples/cifar10/cifar10_circ_solver_lr1.prototxt \
-    --snapshot=examples/cifar10/cifar10_circ_iter_60000.solverstate.h5
+#$TOOLS/caffe.bin train \
+  #  --solver=examples/cifar10/cifar10_circ_solver_lr1.prototxt \
+ #   --snapshot=examples/cifar10/cifar10_circ_iter_60000.solverstate.h5
 
 # reduce learning rate by factor of 10
-$TOOLS/caffe.bin train \
-    --solver=examples/cifar10/cifar10_circ_solver_lr2.prototxt \
-    --snapshot=examples/cifar10/cifar10_circ_iter_65000.solverstate.h5
+#$TOOLS/caffe.bin train \
+ #   --solver=examples/cifar10/cifar10_circ_solver_lr2.prototxt \
+  #  --snapshot=examples/cifar10/cifar10_circ_iter_65000.solverstate.h5
