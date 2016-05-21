d90a8969d049725fadf35fae3cdaf9d455e8bce2
xsnow <xsnow@outlook.com>
2016-05-21 06:12:42 -0400

    Cifar10 models in caffe

--- examples/cifar10/train_circ.sh
+++ examples/cifar10/train_circ.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env sh
 
-TOOLS=./build/tools
+TOOLS=../recaffe/build/tools
 
+#$TOOLS/caffe.bin train \
+#    --solver=examples/cifar10/cifar10_circ_solver.prototxt
+
+# reduce learning rate by factor of 10
 $TOOLS/caffe.bin train \
-    --solver=examples/cifar10/cifar10_circ_solver.prototxt
-
-exit 0
-
-# reduce learning rate by factor of 10
-#$TOOLS/caffe.bin train \
-  #  --solver=examples/cifar10/cifar10_circ_solver_lr1.prototxt \
- #   --snapshot=examples/cifar10/cifar10_circ_iter_60000.solverstate.h5
+    --solver=examples/cifar10/cifar10_circ_solver_lr1.prototxt \
+    --snapshot=examples/cifar10/cifar10_circ_iter_60000.solverstate.h5
 
 # reduce learning rate by factor of 10
-#$TOOLS/caffe.bin train \
- #   --solver=examples/cifar10/cifar10_circ_solver_lr2.prototxt \
-  #  --snapshot=examples/cifar10/cifar10_circ_iter_65000.solverstate.h5
+$TOOLS/caffe.bin train \
+    --solver=examples/cifar10/cifar10_circ_solver_lr2.prototxt \
+    --snapshot=examples/cifar10/cifar10_circ_iter_65000.solverstate.h5
